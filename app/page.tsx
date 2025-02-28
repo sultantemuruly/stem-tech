413dbcc9ce025eaac5ab72f96bfa706292198007
sultantemuruly <sultantemuruly@gmail.com>
2025-02-28 20:08:45 +0500

    feat: update blog on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,26 @@ export default async function Home() {
       <div className="text-md">
         <div className="font-medium">Blogs</div>
         {posts.results.map((post: any) => {
+          console.log(post.properties);
           return (
-            <article key={post.id}>
+            <article key={post.id} className="flex justify-between">
               <Link
                 href={`/blog/${post.properties.slug.rich_text[0].plain_text}`}
+                className="underline"
               >
                 {post.properties.Title.title[0].plain_text}
               </Link>
+              <div>
+                {post.properties.Date?.created_time
+                  ? new Date(
+                      post.properties.Date.created_time
+                    ).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                  : "No Date"}
+              </div>
             </article>
           );
         })}

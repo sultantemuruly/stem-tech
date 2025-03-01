fd17309da49d905a74df55c2036922d016e5e0f5
sultantemuruly <sultantemuruly@gmail.com>
2025-03-01 11:50:59 +0500

    type error fix

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,32 @@ export default async function Home() {
       <About />
       <div className="text-md">
         <div className="font-medium">Blogs</div>
-        {posts.results.map((post: any) => {
-          return (
-            <article key={post.id} className="flex justify-between">
-              <Link
-                href={`/blog/${post.properties.slug.rich_text[0].plain_text}`}
-                className="underline"
-              >
-                {post.properties.Title.title[0].plain_text}
-              </Link>
-              <div>
-                {post.properties.Date?.created_time
-                  ? new Date(
-                      post.properties.Date.created_time
-                    ).toLocaleDateString("en-US", {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    })
-                  : "No Date"}
-              </div>
-            </article>
-          );
-        })}
+        {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          posts.results.map((post: any) => {
+            return (
+              <article key={post.id} className="flex justify-between">
+                <Link
+                  href={`/blog/${post.properties.slug.rich_text[0].plain_text}`}
+                  className="underline"
+                >
+                  {post.properties.Title.title[0].plain_text}
+                </Link>
+                <div>
+                  {post.properties.Date?.created_time
+                    ? new Date(
+                        post.properties.Date.created_time
+                      ).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })
+                    : "No Date"}
+                </div>
+              </article>
+            );
+          })
+        }
       </div>
     </div>
   );
